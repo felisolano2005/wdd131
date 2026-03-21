dbf6fd3edc1779941d7f355b0a6e34a38b73c1aa
felisolano2005 <felisolano2005@gmail.com>
2026-03-21 12:41:28 -0600

    First Part Activity

---
+++ scripts/temples.js
@@ -0,0 +1,16 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const menuButton = document.getElementById("menuButton");
+const navMenu = document.getElementById("navMenu");
+
+menuButton.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+
+    // Toggle icon
+    if (menuButton.textContent === "☰") {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
