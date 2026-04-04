b600b9a4e67ae4da323b7f538d07f28d3eb17b68
felisolano2005 <felisolano2005@gmail.com>
2026-04-04 17:59:22 -0600

    final update

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -5,7 +5,7 @@ const temples = [
     dedicated: "2000, March, 5",
     year: 2000,
     area: 34000,
-    imageUrl: "images/albuquerque.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/albuquerque-new-mexico-temple/albuquerque-new-mexico-temple-56335-main.jpg"
   },
   {
     name: "Arequipa Peru Temple",
@@ -13,7 +13,7 @@ const temples = [
     dedicated: "2019, December, 15",
     year: 2019,
     area: 26900,
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Arequipa_Peru_Temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/arequipa-peru-temple/arequipa-peru-temple-7186-main.jpg"
   },
   {
     name: "Bangkok Thailand Temple",
@@ -21,39 +21,64 @@ const temples = [
     dedicated: "2023, October, 22",
     year: 2023,
     area: 48500,
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Bangkok_Thailand_Temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/bangkok-thailand-temple/bangkok-thailand-temple-40037-main.jpg"
   },
   {
-    name: "Bern Switzerland Temple",
-    location: "Bern, Switzerland",
-    dedicated: "1955, September, 11",
-    year: 1955,
-    area: 35500,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/bern-switzerland-temple/bern-switzerland-temple-54640-thumb.jpg"
+    name: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    year: 2005,
+    area: 11500,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
   },
   {
-    name: "Bogota Colombia Temple",
-    location: "Bogotá, Colombia",
-    dedicated: "1999, April, 24",
-    year: 1999,
-    area: 53500,
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5b/Bogota_Colombia_Temple.jpg"
+    name: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
   },
   {
-    name: "Boise Idaho Temple",
-    location: "Boise, Idaho, USA",
-    dedicated: "1984, May, 25",
-    year: 1984,
-    area: 35800,
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Boise_Idaho_Temple.jpg"
+    name: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
   },
   {
-    name: "Brasilia Brazil Temple",
-    location: "Brasília, Brazil",
-    dedicated: "2023, September, 17",
-    year: 2023,
-    area: 25000,
-    imageUrl: "images/brasilia-temple"
+    name: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    name: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+  },
+  {
+    name: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    name: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
 ];
 
